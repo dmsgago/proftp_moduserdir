f7fcead698eb4a03996c87e9e93bba4bc30527ae
Diego Martín <martin.diego11@hotmail.com>
2016-01-29 14:11:35 +0100

    Programa que crear un nuevo usuario y configura el sitio virtual de Apache, para añadir una redirección para que el usuario pueda accerder mediente un navegador a su página.

---
+++ proftpcreate.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Funciones
+num_argumentos()
+{
+    if [[ $1 -ne "1" ]]
+    then
+	echo "Numero de argumentos inválido: $1"
+	exit
+    fi
+}
+
+# Programa principal
+
+num_argumentos $#
+useradd $1 -m -s /bin/bash -p $(mkpasswd usuario)
+sed 's%</VirtualHost>%        Alias \"/matematicas\" \"/home/$1/ftp/\"\n        <Directory /home/$1/ftp>\n                Options Indexes FollowSymLinks\n       AllowOverride None\n                Require all granted\n        </Directory>\n</VirtualHost>%g' /etc/apache2/sites-available/iesgn.conf
+systemctl restart apache2
